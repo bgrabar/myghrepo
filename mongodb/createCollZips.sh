583902ac63c72aadaa65f4fdb316fcf807f5a316
Bob Grabar <bob.grabar@10gen.com>
2013-08-27 16:32:56 -0400

    update createCollZips.sh

--- mongodb/createCollZips.sh
+++ mongodb/createCollZips.sh
@@ -1,11 +1,20 @@
-for (w=53000;w<60000;w=w+1000) {
+for (w=0;w<99999;w=w+1000) {
 
-  for (x=100;x<1000;x=x+100) {
+  for (x=0;x<1000;x=x+100) {
 
-    for (y=10;y<100;y=y+10) {
+    for (y=0;y<100;y=y+10) {
 
-      for (z=0;z<10;z=z+1) {
+      for (z=1;z<10;z=z+1) {
 
-        db.contacts.insert( { zip : ( w + x + y + z ) , town : "whatever" } ) 
+       switch ((z+3)%3) {
 
-} } } }
+         case 0: tmp = "anytown" ; break;
+
+         case 1: tmp = "smallville" ; break;
+
+         case 2: tmp = "big city" ; break;
+
+       }
+
+       db.numbers_collection.insert( { zip : ( w + x + y + z ) , town : tmp } ) } } } }
+
