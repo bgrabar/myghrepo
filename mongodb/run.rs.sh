64d4b09d3b08b400ddfe82d50570ae84a693900e
Bob Grabar <bob.grabar@10gen.com>
2013-09-12 08:37:37 -0400

    edit to run.rs.sh so all four run

--- mongodb/run.rs.sh
+++ mongodb/run.rs.sh
@@ -4,7 +4,7 @@ mongod --port 34001 --replSet rs --dbpath /data/rs1 --logpath /var/log/mongodb/r
 
 mongod --port 34002 --replSet rs --dbpath /data/rs2 --logpath /var/log/mongodb/rs2.log --nojournal --config /etc/mongodb/null --fork
 
-# mongod --port 34003 --replSet rs --dbpath /data/rs3 --logpath /var/log/mongodb/rs3.log --nojournal --config /etc/mongodb/null --fork
+mongod --port 34003 --replSet rs --dbpath /data/rs3 --logpath /var/log/mongodb/rs3.log --nojournal --config /etc/mongodb/null --fork
 
 mongod --port 34004 --replSet rs --dbpath /data/rs4 --logpath /var/log/mongodb/rs4.log --nojournal --config /etc/mongodb/null --fork
 
