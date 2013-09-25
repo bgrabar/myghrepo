7c287b7e354df4e581bb53d9e34d2c2020ba095b
Bob Grabar <bob.grabar@10gen.com>
2013-09-25 15:15:08 -0400

    correct the db path in the 2.4 replica set scripts

--- mongodb/run.4.rsa.sh
+++ mongodb/run.4.rsa.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mongod --port 34041 --replSet rsa --dbpath /data/rsa1 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa1.24.log --fork --nojournal
+mongod --port 34041 --replSet rsa --dbpath /data/24/rsa1 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa1.24.log --fork --nojournal
 
-mongod --port 34042 --replSet rsa --dbpath /data/rsa2 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa2.24.log --fork --nojournal
+mongod --port 34042 --replSet rsa --dbpath /data/24/rsa2 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa2.24.log --fork --nojournal
 
-mongod --port 34043 --replSet rsa --dbpath /data/rsa3 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa3.24.log --fork --nojournal
+mongod --port 34043 --replSet rsa --dbpath /data/24/rsa3 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa3.24.log --fork --nojournal
 
-mongod --port 34044 --replSet rsa --dbpath /data/rsa4 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa4.24.log --fork --nojournal
+mongod --port 34044 --replSet rsa --dbpath /data/24/rsa4 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa4.24.log --fork --nojournal
