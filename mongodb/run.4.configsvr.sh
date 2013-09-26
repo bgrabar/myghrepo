ca64367dbfd348961fb6ebb4d70b3c00f25d01db
Bob Grabar <bob.grabar@10gen.com>
2013-09-26 10:05:38 -0400

    minor edits to run configsvr scripts

--- mongodb/run.4.configsvr.sh
+++ mongodb/run.4.configsvr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mongod --port 27049 --configsvr --dbpath /data/24/configsvr --logpath /var/log/mongodb/configsvr24.log --nojournal --config /etc/mongodb/null --fork
+mongod --configsvr --port 27049 --dbpath /data/24/configsvr --logpath /var/log/mongodb/configsvr24.log --nojournal --config /etc/mongodb/null --fork
--- mongodb/run.5.configsvr.sh
+++ mongodb/run.5.configsvr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/Users/bob/mongo/mongod --port 27019 --configsvr --dbpath /data/configsvr --logpath /var/log/mongodb/configsvr26.log --nojournal --config /etc/mongodb/null --fork
+/Users/bob/mongo/mongod --configsvr --port 27019 --dbpath /data/configsvr --logpath /var/log/mongodb/configsvr26.log --nojournal --config /etc/mongodb/null --fork
