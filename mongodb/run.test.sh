7e9fe3cbcf6d72af32f021d91dd2b8e1a572eb53
Bob Grabar <bob.grabar@10gen.com>
2013-11-05 12:30:11 -0500

    add run.test.sh script

---
+++ mongodb/run.test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/Users/bob/mongo/mongod --port 34021 --replSet test --dbpath /data/test1 --config /etc/mongodb/null --logpath /var/log/mongodb/test1.log --fork --nojournal
+
+/Users/bob/mongo/mongod --port 34022 --replSet test --dbpath /data/test2 --config /etc/mongodb/null --logpath /var/log/mongodb/test2.log --fork --nojournal
+
