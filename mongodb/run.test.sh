3ced47857a3eab475de38579a05a3bfe0c54b315
Bob Grabar <bob.grabar@10gen.com>
2013-11-12 16:59:41 -0500

    rename dir to bash.odai

--- mongodb/run.test.sh
+++ mongodb/run.test.sh
@@ -4,4 +4,4 @@
 
 /Users/bob/mongo/mongod --port 34022 --replSet test --dbpath /data/test2 --config /etc/mongodb/null --logpath /var/log/mongodb/test2.log --fork --nojournal
 
-# /Users/bob/mongo/mongod --port 34023 --replSet test --dbpath /data/test3 --config /etc/mongodb/null --logpath /var/log/mongodb/test3.log --fork --nojournal
+/Users/bob/mongo/mongod --port 34023 --replSet test --dbpath /data/test3 --config /etc/mongodb/null --logpath /var/log/mongodb/test3.log --fork --nojournal
