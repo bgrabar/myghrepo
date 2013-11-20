a92583d74902fe03b0e29ea3cd11572da8d84e0b
Bob Grabar <bob.grabar@10gen.com>
2013-11-20 13:51:55 -0500

    Add 'auth' script. Edit arbiter script.

--- mongodb/run.arbiter.test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Users/bob/mongo/mongod --port 34024 --replSet test --dbpath /data/test4 --config /etc/mongodb/null --logpath /var/log/mongodb/test4.log --fork --nojournal
---
+++ mongodb/run.auth.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+/Users/bob/mongo/mongod --port 34021 --auth --dbpath /data/auth --config /etc/mongodb/null --logpath /var/log/mongodb/auth.log --fork --nojournal
+
+
+
+
+
+
+
+# authenticate:
+#
+# ./mongo --port 34021 --username "super" --password "pass" --authenticationDatabase "admin"
+#
+#
