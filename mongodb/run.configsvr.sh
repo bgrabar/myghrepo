7dd2823d9cf187a743632a306a4aeb3cd5d73614
Bob Grabar <bob.grabar@10gen.com>
2013-12-04 13:09:42 -0500

    update scripts to run three config servers

--- mongodb/run.configsvr.sh
+++ mongodb/run.configsvr.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-/Users/bob/mongo/mongod --configsvr --port 27019 --dbpath /data/configsvr --logpath /var/log/mongodb/configsvr.log --nojournal --config /etc/mongodb/null --fork
+/Users/bob/mongo/mongod --configsvr --port 27019 --dbpath /data/cs1 --logpath /var/log/mongodb/cs1.log --nojournal --config /etc/mongodb/null --fork
 
+#
+/Users/bob/mongo/mongod --configsvr --port 27020 --dbpath /data/cs2 --logpath /var/log/mongodb/cs2.log --nojournal --config /etc/mongodb/null --fork
 
-
-
+#
+/Users/bob/mongo/mongod --configsvr --port 27021 --dbpath /data/cs3 --logpath /var/log/mongodb/cs3.log --nojournal --config /etc/mongodb/null --fork
 
--- mongodb/run.mongos.sh
+++ mongodb/run.mongos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/Users/bob/mongo/mongos --port 27017 --configdb gromit.local:27019 --logpath /var/log/mongodb/mongos.log --chunkSize 1 --fork
+/Users/bob/mongo/mongos --port 27017 --configdb gromit.local:27019,gromit.local:27020,gromit.local:27021 --logpath /var/log/mongodb/mongos.log --chunkSize 1 --fork
 
 
-#                                    --configdb gromit:27019
+# --configdb gromit:27019
 
 
