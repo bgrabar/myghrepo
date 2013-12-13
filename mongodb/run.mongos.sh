84f546e8f7775a0036e9b60b747ae4922e27dce7
Bob Grabar <bob.grabar@10gen.com>
2013-12-13 15:58:44 -0500

    auth notes added to run.mongos.sh

--- mongodb/run.mongos.sh
+++ mongodb/run.mongos.sh
@@ -3,6 +3,24 @@
 /Users/bob/mongo/mongos --port 27017 --configdb gromit.local:27019,gromit.local:27020,gromit.local:27021 --logpath /var/log/mongodb/mongos.log --chunkSize 1 --fork
 
 
+
+# ???:
+
+# /Users/bob/mongo/mongos --port 27017 --configdb gromit.local:27019,gromit.local:27020,gromit.local:27021 --logpath /var/log/mongodb/mongos.log --chunkSize 1 --fork --keyFile /Users/bob/Desktop/keyfile.txt
+
+# /Users/bob/mongo/mongos --port 27017 --configdb gromit.local:27019,gromit.local:27020,gromit.local:27021 --logpath /var/log/mongodb/mongos.log --chunkSize 1 --fork --keyFile jstests/libs/key1
+
+# /Users/bob/mongo/mongos --port 27017 --configdb gromit.local:27019,gromit.local:27020,gromit.local:27021 --logpath /var/log/mongodb/mongos.log --chunkSize 1 --keyFile jstests/libs/key1
+
+# /Users/bob/mongo/mongos --port 27017 --configdb localhost:30000 --keyFile jstests/libs/key1 --chunkSize 50 --setParameter enableTestCommands=1
+
+
+
+
 # --configdb gromit:27019
+
+
+#  ./mongo -u "recordsUserAdmin" -p "password" --authenticationDatabase "mydb"
+
 
 
