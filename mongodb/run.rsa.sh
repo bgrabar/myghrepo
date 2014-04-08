988432533c319e346ca5376aad2230092f28c48c
Bob Grabar <bob.grabar@10gen.com>
2014-04-08 18:00:49 -0400

    modified scripts and config files

--- mongodb/run.rsa.sh
+++ mongodb/run.rsa.sh
@@ -8,5 +8,14 @@
 
 
 
+# Does not use config file:
+
+# /Users/bob/mongo/mongod --port 34001 --replSet rsa --dbpath /data/rsa1 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa1.log
+
+# /Users/bob/mongo/mongod --port 34002 --replSet rsa --dbpath /data/rsa2 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa2.log
+
+# /Users/bob/mongo/mongod --port 34003 --replSet rsa --dbpath /data/rsa3 --config /etc/mongodb/null --logpath /var/log/mongodb/rsa3.log
+
+
 
 
