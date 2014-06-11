5659c56e41ed6c7ea89ef3e6a0d94263f3a9470b
Bob Grabar <bob.grabar@10gen.com>
2014-06-10 20:57:54 -0400

    Add the rsx replica set Note that this calls the current stable version   of mongod, not a development version.

---
+++ mongodb/run.rsx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mongod --port 34081 --config /etc/mongodb/config-files/rsx81.conf
+
+mongod --port 34082 --config /etc/mongodb/config-files/rsx82.conf
+
+mongod --port 34083 --config /etc/mongodb/config-files/rsx83.conf
+
