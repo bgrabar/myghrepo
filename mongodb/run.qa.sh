d376572081668d093d9d141ddb4997ce1628ca0d
Bob Grabar <bob.grabar@10gen.com>
2014-08-21 14:24:42 -0400

    renamed qa-deployment files

--- mongodb/run.qa.sh
+++ mongodb/run.qa.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mongod --port 54001 --config /etc/mongodb/config-files/rsa-qa-1.conf
-mongod --port 54002 --config /etc/mongodb/config-files/rsa-qa-2.conf
+mongod --port 54001 --config /etc/mongodb/config-files/qa-rsa-1.conf
+mongod --port 54002 --config /etc/mongodb/config-files/qa-rsa-2.conf
 
-mongod --port 54006 --config /etc/mongodb/config-files/rsb-qa-1.conf
-mongod --port 54007 --config /etc/mongodb/config-files/rsb-qa-2.conf
+mongod --port 54006 --config /etc/mongodb/config-files/qa-rsb-1.conf
+mongod --port 54007 --config /etc/mongodb/config-files/qa-rsb-2.conf
 
-mongod --configsvr --port 54019 --config /etc/mongodb/config-files/cs-qa-1.conf
+mongod --configsvr --port 54019 --config /etc/mongodb/config-files/qa-cs-1.conf
 
 mongos --port 54017 --configdb gromit.local:54019 --config /etc/mongodb/config-files/mongos-qa.conf
 
