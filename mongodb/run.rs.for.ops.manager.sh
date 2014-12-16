a3f7a70b540d9d68eda12ac04f2cb3abebcf5a2f
Bob Grabar <bob.grabar@10gen.com>
2014-12-16 12:12:24 -0500

    add script to run rs for Ops Manager

---
+++ mongodb/run.rs.for.ops.manager.sh
@@ -0,0 +1,13 @@
+# run a replica set to add to my own deployed and running Ops Manager:
+
+mongod --port 34071 --dbpath /data/rsz-71 --logpath /var/log/mongodb/rsz-71.log --fork --nojournal --replSet rsz
+
+mongod --port 34072 --dbpath /data/rsz-72 --logpath /var/log/mongodb/rsz-72.log --fork --nojournal --replSet rsz
+
+mongod --port 34073 --dbpath /data/rsz-73 --logpath /var/log/mongodb/rsz-73.log --fork --nojournal --replSet rsz
+
+mongod --port 34074 --dbpath /data/rsz-74 --logpath /var/log/mongodb/rsz-74.log --fork --nojournal --replSet rsz
+
+
+
+
