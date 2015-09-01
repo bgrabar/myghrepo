ae068fbd8d8961bacc9796fd7ee102873457a60d
Bob Grabar <bob.grabar@10gen.com>
2015-09-01 09:19:08 -0400

    add comments

--- udacity_course_36/lesson_1/break_time.py
+++ udacity_course_36/lesson_1/break_time.py
@@ -1,5 +1,9 @@
-import time
-import webbrowser
+# Standalone program to wait for a period of time then open a web browser.
+# and play a video. Loops through this three times.
+
+
+import time          # for time.sleep function
+import webbrowser    # for webbrowser.open function
 
 total_breaks = 3
 counter = 1
@@ -13,6 +17,7 @@ while(counter <= total_breaks):
   print counter
 
 print("The time is " + time.ctime())
+
 
 
 
