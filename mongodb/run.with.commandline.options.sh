295fb6eb9de163b12363f45d435664f72f5a8d2a
Bob Grabar <bob.grabar@10gen.com>
2015-09-30 13:39:17 -0400

    updates to mongodb scripts & config file

--- mongodb/run.with.commandline.options.sh
+++
@@ -1,8 +0,0 @@
-# run a replica set with command-line options
-
-mongod --port 34051 --dbpath /data/rsz-51 --logpath /var/log/mongodb/rsz-51.log --fork --nojournal --replSet rsz
-
-mongod --port 34052 --dbpath /data/rsz-52 --logpath /var/log/mongodb/rsz-52.log --fork --nojournal --replSet rsz
-
-mongod --port 34053 --dbpath /data/rsz-53 --logpath /var/log/mongodb/rsz-53.log --fork --nojournal --replSet rsz
-
