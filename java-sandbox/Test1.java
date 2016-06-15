feda41a4a2f2cf7a081d7d9c395a20e30863b142
Bob Grabar <bob.grabar@10gen.com>
2016-06-15 14:14:24 -0400

    create java-sandbox

---
+++ java-sandbox/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+  
+  public static void main(String[] args) {
+    
+    String[] array = {"One", "Two", "Three", "Four", "Five" };
+    for (String var : array)
+      System.out.println("Element: " + var);
+    
+  }
+}
