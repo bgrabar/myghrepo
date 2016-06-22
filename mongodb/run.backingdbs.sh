5ae9da4181da22eb0b7f6e76cb2a55606115ac17
Bob Grabar <bob.grabar@10gen.com>
2016-06-22 10:45:09 -0400

    edits to 'run mongodb' scripts

--- mongodb/run.backingdbs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mongod --port 27017 --dbpath /data/backingdb1 --logpath /var/log/mongodb/backingdb1.log --fork --nojournal
-
-mongod --port 27018 --dbpath /data/backingdb2 --logpath /var/log/mongodb/backingdb2.log --fork --nojournal
--- mongodb/run.rsa.with.commandline.options.sh
+++ mongodb/run.rsa.with.commandline.options.sh
@@ -2,9 +2,9 @@
 
 # run a replica set with command-line options
 
-mongod --port 34001 --dbpath /data/rsa1 --logpath /var/log/mongodb/rsa1.log --fork --nojournal --replSet rsa
+mongod --port 34001 --dbpath /data/manually-run-instances/rsa1 --logpath /var/log/mongodb/rsa1.log --fork --nojournal --replSet rsa
 
-mongod --port 34002 --dbpath /data/rsa2 --logpath /var/log/mongodb/rsa2.log --fork --nojournal --replSet rsa
+mongod --port 34002 --dbpath /data/manually-run-instances/rsa2 --logpath /var/log/mongodb/rsa2.log --fork --nojournal --replSet rsa
 
-mongod --port 34003 --dbpath /data/rsa3 --logpath /var/log/mongodb/rsa3.log --fork --nojournal --replSet rsa
+mongod --port 34003 --dbpath /data/manually-run-instances/rsa3 --logpath /var/log/mongodb/rsa3.log --fork --nojournal --replSet rsa
 
