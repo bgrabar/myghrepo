308c855c8a325a4c97d974b3dd2d588b91d76525
Bob Grabar <bob.grabar@10gen.com>
2016-06-28 10:03:03 -0400

    Remove script to run automation agent

--- mongodb/run.automation.agent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# run Automation Agent for local install of Cloud Manager
-
-
-nohup /opt/mongodb-mms-automation-agent --config=local.config >> /var/log/mongodb-mms-automation/automation-agent.log 2>&1 &
