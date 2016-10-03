f5a9d5dc6e57cc49de979cb1ac60e70deb328190
Bob Grabar <bob.grabar@10gen.com>
2016-10-03 15:47:44 -0400

    minor

--- mongodb/run.standalone.sh
+++ mongodb/run.standalone.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+mongod --port 35017 --config /Users/bob/myrepo/mongodb/config-files/standalone.conf
 
 
-# Runs the compiled 'mongod' in
-# /Users/bob/mongo
-
-/Users/bob/mongo/mongod --port 35017 --config /Users/bob/myrepo/mongodb/config-files/standalone.conf
+# Runs the compiled 'mongod' in /Users/bob/mongo:
+# /Users/bob/mongo/mongod --port 35017 --config /Users/bob/myrepo/mongodb/config-files/standalone.conf
