b326afa9f34c53cbb5232eedeb4b84809baaf2a4
Bob Grabar <bob.grabar@10gen.com>
2016-10-17 12:31:41 -0400

    reorganizing

--- code/python/20150909.py
+++
@@ -1,9 +0,0 @@
-
-def double_triple(x,y):
-	num=x+x
-	num2=3*y
-	print num
-	print num2
-
-
-double_triple(5,7)
---
+++ mongodb-scripts/run.standalone.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mongod --port 35017 --config /Users/bob/myrepo/mongodb-scripts/config-files/standalone.conf
+
+
+# To run the compiled 'mongod' in /Users/bob/mongo:
+# /Users/bob/mongo/mongod --port 35017 --config /Users/bob/myrepo/mongodb-scripts/config-files/standalone.conf
