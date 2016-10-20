c48ccaf255d0c977762678c105a0b6a6a8a8bed8
Bob Grabar <bob.grabar@10gen.com>
2016-10-20 18:38:38 -0400

    add comment to concat2.sh

--- yl-project/src/bash/concat2.sh
+++ yl-project/src/bash/concat2.sh
@@ -10,7 +10,14 @@
 # find . -type f -name "*.zip" -exec /gds_02/data/concat2.sh {} \;
 
 EXPANDED_DIR=/gds_02/data/texts/uc1/
+# should above be:
+# EXPANDED_DIR=/gds_02/data/uc1/
+
 ZIP_RELATIVE_PATH=${1:2}
+# note: {1:2} above means take the first input and start with position 2.
+# note: {1:2:7} above means take the first input, start with position 2, and go 7 characters.
+# note: {1:2:1} above means take the first input, start with position 2, and go 1 character.
+
 
 unzip -p $ZIP_RELATIVE_PATH > ${EXPANDED_DIR}${ZIP_RELATIVE_PATH}.txt
 # note: the -p option extracts files to stdout
